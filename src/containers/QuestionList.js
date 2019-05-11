bb76ac423c03aa5546a834080e94125ba28378dd
Pete Eichman <peichman14@gmail.com>
2019-05-11 16:15:06 -0400

    refactored questionlist container to map props to state and update properly.

--- src/containers/QuestionList.js
+++ src/containers/QuestionList.js
@@ -4,26 +4,32 @@ import Question from '../components/Question'
 class QuestionList extends React.Component {
   constructor(props){
     super(props);
-
-    this.state = {
-      value: null,
-      domain: null
-    }
+    this.state = {};
     this.handleFormSubmit = this.handleFormSubmit.bind(this)
+  }
+  componentDidMount(){
+    let questionsArray = this.props.questions.map((question, index) => {
+      return ({id: index, value: '', domain: ''});
+    });
+    let questionsObject = Object.assign({}, questionsArray);
+    this.setState({ questions: questionsObject });
   }
 
   handleFormSubmit(payload) {
-    this.setState({...payload})
+    this.setState({
+      questions: {
+        ...this.state.questions,
+        [payload.id]: {...payload}
+      }
+    });
   }
 
-  render(){
-    console.log(this.state);
-    let questions = this.props.questions.map (question => {
-
+  mapQuestions(){
+    let questions = this.props.questions.map ((question, index) => {
       return (
         <Question
           key={question.num}
-          id={question.num}
+          id={index}
           text={question.text}
           domain={question.domain}
           handleClick={this.handleQuestionClick}
@@ -31,11 +37,17 @@ class QuestionList extends React.Component {
         />
       )
     });
-
     return (
       <div>
         {questions}
       </div>
+    );
+  };
+
+  render(){
+    console.log(this.state);
+    return (
+      this.mapQuestions()
     );
   }
 }
