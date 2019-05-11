69a7f84f38ae16c73bc9ead1adcf4920c8d91d04
Travis Nelson <tnelson17@gmail.com>
2019-05-11 14:08:33 -0400

    expanded questionlist and question

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,19 +1,54 @@
 import React from 'react';
 
-const Question = props => {
-  return (
-    <>
-      <div> {props.text} </div>
-      <select onChange={props.handleClick} name={props.domain}>
-        <option>---</option>
-        <option value={5}>Strongly Agree</option>
-        <option value={4}>Moderately Agree</option>
-        <option value={3}>Neither Agree nor Disagree</option>
-        <option value={2}>Moderately Disagree</option>
-        <option value={1}>Strongly Disagree</option>
-      </select>
-    </>
-  )
-}
+class Question extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      clicked: false,
+      submitted: false,
+      value: '',
+      previousValue: '',
+      domain: this.props.domain,
+      id: this.props.id
+    }
+    this.handleChange = this.handleChange.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this)
+  }
+
+  handleChange(event) {
+    this.setState({
+      clicked: true,
+      value: parseInt(event.target.value) })
+  }
+
+  handleSubmit(event) {
+    event.preventDefault()
+    this.props.handleFormSubmit({
+      value: this.state.value,
+      domain: this.state.domain
+    })
+    this.setState({
+      submitted: true,
+      previousValue: this.state.value
+    })
+  }
+
+  render(){
+    console.log(this.state);
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <div> {this.props.text} </div>
+        <select onChange={this.handleChange}name={this.props.id}>
+          <option value={0}>---</option>
+          <option value={4}>Strongly Agree</option>
+          <option value={3}>Moderately Agree</option>
+          <option value={2}>Neither Agree nor Disagree</option>
+          <option value={1}>Moderately Disagree</option>
+          <option value={0}>Strongly Disagree</option>
+        </select>
+        <button/>
+      </form>
+    )}
+  }
 
 export default Question;
