cf676fa73133763baf1816823d29eb71da4c5ea8
Graham Gilpatric <grahamwithag@gmail.com>
2019-05-16 20:52:46 -0400

    made app stateful

--- src/containers/App.js
+++ src/containers/App.js
@@ -2,15 +2,24 @@ import React from 'react';
 import QuestionList from './QuestionList';
 import data from '../data.json'
 
-const App = () => {
-  return (
-    <div>
-      <h2> Five Factor </h2>
-      <QuestionList
-        questions = {data}
-      />
-    </div>
-  );
+class App extends React.Component {
+  constructor(){
+    super()
+    this.state = {
+      submitted: false,
+      score: {}
+    }
+  }
+  render() {
+    return (
+      <div>
+        <h2> Five Factor </h2>
+        <QuestionList
+          questions = {data}
+        />
+      </div>
+    );
+  }
 };
 
 export default App;
