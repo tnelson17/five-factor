1a57750d8fb53d2a67d592f83cc735eb695c4458
Travis Nelson <tnelson17@gmail.com>
2019-05-23 21:19:49 -0400

    added logic to App to conditionally render ScoreDisplay versus QuestionList

---
+++ src/containers/ScoreDisplay.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+class ScoreDisplay extends React.Component {
+  constructor(){
+    super()
+    this.state = {}
+  }
+  render(){
+    return(
+      <div>
+      </div>
+    )
+  }
+}
+
+export default ScoreDisplay;
