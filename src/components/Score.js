8e0c13895a82353bafbf652057db11143b08917f
Graham Gilpatric <grahamwithag@gmail.com>
2019-06-06 22:18:53 -0400

    Score.js displays descriptive text based on score from description.js

--- src/components/Score.js
+++ src/components/Score.js
@@ -1,9 +1,28 @@
 import React from 'react';
-import { O, C, E, A, N } from '../descriptions.js'
+import descriptions from '../descriptions.js'
 
 const Score = (props) => {
+  let description = ''
+  let percentScore = Math.round((props.score / props.max)*100)
+
+
+    if (percentScore <= 20){
+      description = descriptions[props.domainKey].low
+    } else if (percentScore <= 40){
+      description = descriptions[props.domainKey].moderateLow
+    } else if (percentScore <= 60){
+      description = descriptions[props.domainKey].moderate
+    } else if (percentScore <= 80){
+      description = descriptions[props.domainKey].moderateHigh
+    } else {
+      description = descriptions[props.domainKey].high
+    }
+
   return (
+    <>
       <div>Your score in the dimension of {props.domain} is {props.score} out of {props.max}</div>
+      <div>{description}</div>
+    </>
   )
 }
 
