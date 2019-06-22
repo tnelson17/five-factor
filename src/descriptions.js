d76ff8de58b9d3202f89895fc8f28be9bad37b13
Travis Nelson <tnelson17@gmail.com>
2019-06-22 14:30:42 -0400

    expanded on descriptions

--- src/descriptions.js
+++ src/descriptions.js
@@ -1,38 +1,63 @@
 const descriptions = {
   O: {
-    low: "ur not open lmao",
-    moderateLow: "ur not open lmao",
-    moderate: "ur kinda open lmao",
-    moderateHigh: "ur kinda open lmao",
-    high: "ur totally open bro!"
+    low: `You are very rarely playful, curious, imaginative, creative,
+    open-minded, novelty-seeking, forward-looking`,
+    moderateLow: `You are seldom playful, curious, imaginative, creative,
+    open-minded, novelty-seeking, forward-looking`,
+    moderate: `You are fairly playful, curious, imaginative, creative,
+    open-minded, novelty-seeking, forward-looking`,
+    moderateHigh: `You are quite playful, curious, imaginative, creative,
+    open-minded, novelty-seeking, forward-looking`,
+    high: `You are extremely playful, curious, imaginative, creative,
+    open-minded, novelty-seeking, forward-looking!`
   },
   C: {
-    low: "ur not conscientious lmao",
-    moderateLow: "ur not conscientious lmao",
-    moderate: "ur kinda conscientious lmao",
-    moderateHigh: "ur kinda conscientious lmao",
-    high: "ur totally conscientious bro!"
+    low: `You are very rarely orderly, committed, confident
+    (with respect to a sense of mastery), achievement-oriented, reliable, self-disciplined`,
+    moderateLow: `You are seldom orderly, committed, confident
+    (with respect to a sense of mastery), achievement-oriented, reliable, self-disciplined`,
+    moderate: `You are fairly orderly, committed, confident
+    (with respect to a sense of mastery), achievement-oriented, reliable, self-disciplined`,
+    moderateHigh: `You are quite orderly, committed, confident
+    (with respect to a sense of mastery), achievement-oriented, reliable, self-disciplined`,
+    high: `You are extremely orderly, committed, confident
+    (with respect to a sense of mastery), achievement-oriented, reliable, self-disciplined`
   },
   E: {
-    low: "ur not extroverted lmao",
-    moderateLow: "ur not extroverted lmao",
-    moderate: "ur kinda extroverted lmao",
-    moderateHigh: "ur kinda extroverted lmao",
-    high: "ur totally extroverted bro!"
+    low: `You are very rarely active, energetic, thrill-seeking,
+    enthusiastic, assertive, interactive, friendly.`,
+    moderateLow: `You are seldom active, energetic, thrill-seeking,
+    enthusiastic, assertive, interactive, friendly.`,
+    moderate: `You are fairly active, energetic, thrill-seeking,
+    enthusiastic, assertive, interactive, friendly.`,
+    moderateHigh: `You are quite active, energetic, thrill-seeking,
+    enthusiastic,assertive, interactive, friendly.`,
+    high: `You are extremely active, energetic, thrill-seeking,
+    enthusiastic, assertive, interactive, friendly.`
   },
   A: {
-    low: "ur not agreeable lmao",
-    moderateLow: "ur not agreeable lmao",
-    moderate: "ur kinda agreeable lmao",
-    moderateHigh: "ur kinda agreeable lmao",
-    high: "ur totally agreeable bro!"
+    low: `You are very rarely accommodating, loyal/trusting, compassionate,
+    altruistic, steady, cooperative, forgiving/tolerant.`,
+    moderateLow: `You are seldom accommodating, loyal/trusting, compassionate,
+    altruistic, steady, cooperative, forgiving/tolerant.`,
+    moderate: `You are fairly accommodating, loyal/trusting, compassionate,
+    altruistic, steady, cooperative, forgiving/tolerant.`,
+    moderateHigh: `You are quite accommodating, loyal/trusting, compassionate,
+    altruistic, steady, cooperative, forgiving/tolerant.`,
+    high: `You are extremely accommodating, loyal/trusting, compassionate,
+    altruistic, steady, cooperative, forgiving/tolerant.`
   },
   N: {
-    low: "ur not neurotic lmao",
-    moderateLow: "ur not neurotic lmao",
-    moderate: "ur kinda neurotic lmao",
-    moderateHigh: "ur kinda neurotic lmao",
-    high: "ur totally neurotic bro!"
+    low: `You are extremely calm, even-tempered, positive,
+    resilient/robust, deliberate, easy-going, regulated.`,
+    moderateLow: `You are quite calm, even-tempered, positive,
+    resilient/robust, deliberate, easy-going, regulated.`,
+    moderate: `You are fairly calm, even-tempered, positive,
+    resilient/robust, deliberate, easy-going, regulated.`,
+    moderateHigh: `You are seldom calm, even-tempered, positive,
+    resilient/robust, deliberate, easy-going, regulated.`,
+    high: `You are very rarely calm, even-tempered, positive,
+    resilient/robust, deliberate, easy-going, regulated.`
   }
 }
 
